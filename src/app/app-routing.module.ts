0be6566dd44085b35c886655b9b96bd0fb7861a5
anamika1689 <singhanamika1689@gmail.com>
2020-10-11 19:50:31 +0530

    Second Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import {OrdersComponent} from './orders/orders.component';
 import {OrderDetailsComponent} from './order-details/order-details.component';
 
 const routes: Routes = [
+  {path:'', redirectTo:'orders',pathMatch:'full'},
   {path:'orders', component:OrdersComponent},
-  {path:'orderDetails/:orderId', component:OrderDetailsComponent}
- 
+  {path:'orderDetails/:orderId', component:OrderDetailsComponent},
+ {path:'**',redirectTo:'orders' }
 
 ];
 
